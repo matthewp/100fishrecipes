cf9e6560fdb06f0bb3a4cb656d66bc2d2f232340
Matthew Phillips <matthew@matthewphillips.info>
2013-07-14 12:24:27 -0400

    Making way for misaki version

--- Readme.md
+++
@@ -1,7 +0,0 @@
-# 100 Fish Recipes
-
-This is a blog of mine!
-
-# License
-
-See the LICENSE file
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-ARCHIVE=100fishes.tar.gz
-
-jekyll --no-auto --no-server
-tar -cvzf /tmp/$ARCHIVE _site/
-scp /tmp/$ARCHIVE matthew@$1:/tmp/$ARCHIVE
-ssh matthew@$1 'cd /tmp; tar xvf 100fishes.tar.gz; rm -rf ~/www/100fishes; mkdir ~/www/100fishes; mv -f /tmp/_site/* ~/www/100fishes'
-rm /tmp/$ARCHIVE
-
